595f1a7eeead5d9d0c7a7f19bb2e83b100db1083
theuslino <matheusdossantos3157@gmail.com>
2025-03-11 16:21:56 -0300

    feat: adicionando um timer de 25 minutos, todos os botões funcionam

--- Pomodoro/app/src/main/java/com/example/pomodoro/MainActivity.kt
+++ Pomodoro/app/src/main/java/com/example/pomodoro/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.pomodoro
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -33,35 +34,55 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun PomodoroScreen(){
-    var timeLeft by remember { mutableStateOf("25:00")}
+    var timeLeft by remember { mutableStateOf(25 * 60 * 1000L)} // 25 minutos em milissegundos.
+    var isRunning by remember { mutableStateOf(false) }
+    var timer: CountDownTimer? by remember { mutableStateOf(null) }
+
+    fun startTimer(){
+        if (!isRunning) {
+            isRunning = true
+            timer = object : CountDownTimer(timeLeft, 1000){
+                override fun onTick(millisUntilFinished: Long) {
+                    timeLeft = millisUntilFinished
+                }
+
+                override fun onFinish() {
+                    isRunning = false
+                }
+            }.start()
+        }
+    }
+
+    fun pauseTimer(){
+        timer?.cancel()
+        isRunning = false
+    }
+
+    fun resetTimer(){
+        timer?.cancel()
+        timeLeft = 25 * 60 * 1000L
+        isRunning = false
+    }
 
     Column (
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = timeLeft, fontSize = 48.sp)
+        Text(text = formatTime(timeLeft), fontSize = 48.sp)
         Spacer(modifier = Modifier.height(20.dp))
         Row {
-            Button(onClick = { /* Iniciar Timer */ }) {
-                Text("Iniciar")
-            }
-            Spacer(modifier = Modifier.height(10.dp))
-            Button(onClick = { /* Pausar Timer */}) {
-                Text("Pausar")
-            }
+            Button(onClick = { startTimer() }) { Text("Iniciar") }
             Spacer(modifier = Modifier.width(10.dp))
-            Button(onClick = { /* Reiniciar Timer */}) {
-                Text("Reiniciar")
-            }
+            Button(onClick = { pauseTimer() }) { Text("Pausar") }
+            Spacer(modifier = Modifier.width(10.dp))
+            Button(onClick = { resetTimer() }) { Text("Reiniciar") }
         }
     }
 }
 
-/*@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PomodoroTheme {
-        Greeting("Android")
-    }
-}*/
+fun formatTime(millis: Long): String{
+    val minutes = (millis / 1000) / 60
+    val seconds = (millis / 1000) % 60
+    return String.format("%02d:%02d", minutes, seconds)
+}
