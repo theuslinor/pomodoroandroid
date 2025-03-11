01c9276f3d932c60661c51b9a926f5d9118c8b9b
theuslino <matheusdossantos3157@gmail.com>
2025-03-11 17:36:57 -0300

    feat: adicionando pausa curta e longa, ainda com tempos curtos para testes

--- Pomodoro/app/src/main/java/com/example/pomodoro/MainActivity.kt
+++ Pomodoro/app/src/main/java/com/example/pomodoro/MainActivity.kt
@@ -7,10 +7,20 @@ import android.os.Handler
 import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -35,14 +45,26 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+enum class TimerMode(val duration: Long) {
+    POMODORO( 25 * 60 * 1000L),
+    PAUSA_CURTA(5 * 60 * 1000L),
+    PAUSA_LONGA(15 * 60 * 1000L),
+    POMODORO_TESTE(10 * 1000L),
+    PAUSA_CURTA_TESTE(5 * 1000L),
+    PAUSA_LONGA_TESTE(10 * 1000L)
+
+}
+
 @Composable
 fun PomodoroScreen(
     onStart: () -> Unit,
     onPause: () -> Unit,
     onFinish: () -> Unit
 ){
-    var timeLeft by remember { mutableStateOf(/*25 * 60 * 1000L*/ 10 * 1000L)} // 25 minutos em milissegundos.
+    var selectedMode by remember { mutableStateOf(TimerMode.POMODORO_TESTE) }
+    var timeLeft by remember { mutableStateOf(TimerMode.POMODORO_TESTE.duration)}
     var isRunning by remember { mutableStateOf(false) }
+    var isFirstCycleDone by remember { mutableStateOf(false) }
     var timer: CountDownTimer? by remember { mutableStateOf(null) }
 
     fun startTimer(){
@@ -58,6 +80,7 @@ fun PomodoroScreen(
 
                     override fun onFinish() {
                         isRunning = false
+                        isFirstCycleDone = true
                         onFinish()
                     }
                 }.start()
@@ -77,6 +100,13 @@ fun PomodoroScreen(
         isRunning = false
     }
 
+    fun changeMode(mode: TimerMode) {
+        timer?.cancel()
+        selectedMode = mode
+        timeLeft = mode.duration
+        isRunning = false
+    }
+
     Column (
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
@@ -84,6 +114,17 @@ fun PomodoroScreen(
     ) {
         Text(text = formatTime(timeLeft), fontSize = 48.sp)
         Spacer(modifier = Modifier.height(20.dp))
+
+        if (isFirstCycleDone){
+            Row {
+                Button(onClick = { changeMode(TimerMode.POMODORO) }) { Text("Pomodoro") }
+                Spacer(modifier = Modifier.width(10.dp))
+                Button(onClick = { changeMode(TimerMode.PAUSA_CURTA_TESTE) }) { Text("Pausa Curta") }
+                Spacer(modifier = Modifier.width(10.dp))
+                Button(onClick = { changeMode(TimerMode.PAUSA_LONGA_TESTE) }) { Text("Pausa Longa") }
+            }
+        }
+
         Row {
             Button(onClick = { startTimer() }) { Text("Iniciar") }
             Spacer(modifier = Modifier.width(10.dp))
